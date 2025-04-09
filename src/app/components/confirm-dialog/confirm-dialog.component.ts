285ec94bdd33cbd06720160e5a315b90c2fde86f
AlefLaniel <alef6940@gmail.com>
2025-04-09 08:44:26 -0300

    feat: adicionar comentários explicativos no componente de diálogo de confirmação

--- src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -12,15 +12,18 @@ import { MatDialogActions } from '@angular/material/dialog';
   styleUrl: './confirm-dialog.component.css'
 })
 export class ConfirmDialogComponent {
+  // Variável para armazenar a mensagem do diálogo
   constructor(
     public dialogRef: MatDialogRef<ConfirmDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public message: string
   ) {}
 
+  // Método para fechar o diálogo com confirmação
   onConfirm(): void {
     this.dialogRef.close(true);
   }
 
+  // Método para fechar o diálogo sem confirmação
   onCancel(): void {
     this.dialogRef.close(false);
   }
